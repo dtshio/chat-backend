3bdc3b4c37a54521c5b05652e5cc96a31b362f3e
datsfilipe <datsfilipe.foss@proton.me>
2024-01-12 17:37:52 -0300

    refactor: rename middleware to corsMiddleware

---
+++ pkg/application/server/corsMiddleware.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"net/http"
+)
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		allowedOrigins := []string{
+			"http://localhost:5173",
+		}
+
+		origin := r.Header.Get("Origin")
+
+		allowed := false
+		for _, allowedOrigin := range allowedOrigins {
+			if allowedOrigin == origin {
+				allowed = true
+				break
+			}
+		}
+
+		if allowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		if !allowed {
+			http.Error(w, "Not allowed", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
