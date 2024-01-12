19e79edb8f4283600af917aaf4e2740ea09b08c2
datsfilipe <datsfilipe.foss@proton.me>
2024-01-12 18:37:56 -0300

    feat: add pagination as core function

---
+++ pkg/core/pagination.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"gorm.io/gorm"
+)
+
+type Pagination struct {
+	DB *gorm.DB
+	PageSize int
+	PageNumber int
+}
+
+type GetMessagesPayload struct {
+	ChannelID string `json:"channel_id"`
+	ID string `json:"id"`
+	Page int `json:"page"`
+}
+
+func NewPagination(db *gorm.DB, pageSize, pageNumber int) *Pagination {
+	return &Pagination{
+		DB: db,
+		PageSize: pageSize,
+		PageNumber: pageNumber,
+	}
+}
+
+func (p *Pagination) ReversePaginate(model interface{}, orderColumn string) ([]interface{}, error) {
+	offset := p.PageNumber * p.PageSize
+
+	var results []interface{}
+	err := p.DB.Model(model).Order(orderColumn + " DESC").Offset(offset).Limit(p.PageSize).Find(&results).Error
+
+	return results, err
+}
