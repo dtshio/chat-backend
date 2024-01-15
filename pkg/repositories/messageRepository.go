503f32f19729684462d634e54fa6222d95ca2075
datsfilipe <datsfilipe.foss@proton.me>
2024-01-14 21:50:41 -0300

    fix: messages history in wrong order

--- pkg/repositories/messageRepository.go
+++ pkg/repositories/messageRepository.go
@@ -35,14 +35,17 @@ func (mr *MessageRepository) GetMessages(db *gorm.DB, data interface{}) (interfa
     var messages []models.Message
     offset := pagination.PageSize * (pagination.PageNumber)
     
-    db = db.Table("messages").Order("created_at ASC").Offset(offset).Limit(pagination.PageSize)
-    
+    db = db.Table("messages").Order("created_at DESC").Offset(offset).Limit(pagination.PageSize)
     db = db.Where("channel_id = ?", pagination.Key)
     
     err := db.Find(&messages).Error
     if err != nil {
         return nil, fmt.Errorf("Error getting Messages: %v", err)
     }
+
+	for i, j := 0, len(messages)-1; i < j; i, j = i+1, j-1 {
+		messages[i], messages[j] = messages[j], messages[i]
+	}
 
     return messages, err
 }
