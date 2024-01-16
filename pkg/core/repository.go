ac1b1cdc6234eb682841325fe5ac6a6964c01107
datsfilipe <datsfilipe.foss@proton.me>
2024-01-15 21:34:04 -0300

    feat: embed errors in base repository and service

--- pkg/core/repository.go
+++ pkg/core/repository.go
@@ -1,22 +1,35 @@
 package core
 
 import (
-	"go.uber.org/zap"
-
 	"gorm.io/gorm"
 )
 
 type RepositoryMethod func(*gorm.DB, interface{}) (interface{}, error)
 
 type Repository struct {
-	logger zap.Logger
 	target interface{}
 	methods []RepositoryMethod
+
+	GenError func(string, interface{}) error
+	InvalidData string
+	CreatingError string
+	GettingError string
+	DuplicateError string
+	UpdateError string
+	DeleteError string
 }
 
 func NewRepository(model interface{}, methods []RepositoryMethod) *Repository {
 	return &Repository{
 		target: model,
 		methods: methods,
+
+		GenError: GenError,
+		InvalidData: InvalidData,
+		CreatingError: CreatingError,
+		GettingError: GettingError,
+		DuplicateError: DuplicateError,
+		UpdateError: UpdateError,
+		DeleteError: DeleteError,
 	}
 }
--- pkg/core/service.go
+++ pkg/core/service.go
@@ -1,15 +1,33 @@
 package core
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type ServiceMethod func(*gorm.DB, interface{}) (interface{}, error)
 
 type Service struct {
 	methods []ServiceMethod
+
+	GenError func(string, interface{}) error
+	InvalidData string
+	CreatingError string
+	GettingError string
+	DuplicateError string
+	UpdateError string
+	DeleteError string
 }
 
 func NewService(methods []ServiceMethod) *Service {
 	return &Service{
-		methods,
+		methods: methods,
+
+		GenError: GenError,
+		InvalidData: InvalidData,
+		CreatingError: CreatingError,
+		GettingError: GettingError,
+		DuplicateError: DuplicateError,
+		UpdateError: UpdateError,
+		DeleteError: DeleteError,
 	}
 }
