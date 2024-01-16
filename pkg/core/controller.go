9755f6d4226745dad104cd5f106c67407ea59b7f
datsfilipe <datsfilipe.foss@proton.me>
2024-01-15 21:32:02 -0300

    feat: add utility functions to controller base
    - Response
    - IsAllowedMethod
    - IsAuthorized
    - GetPayload

--- pkg/core/controller.go
+++ pkg/core/controller.go
@@ -1,8 +1,11 @@
 package core
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 
+	"github.com/datsfilipe/pkg/application/auth"
 	"gorm.io/gorm"
 )
 
@@ -15,6 +18,85 @@ type Controller struct {
 
 func (c *Controller) SetDB(db *gorm.DB) {
 	c.db = db
+}
+
+type Map map[string]interface{}
+
+func (c *Controller) GetPayload(r *http.Request) Map {
+	payload := *new(Map)
+
+	var raw json.RawMessage
+	err := json.NewDecoder(r.Body).Decode(&raw)
+	if err != nil {
+		return nil
+	}
+
+	err = json.Unmarshal(raw, &payload)
+	if err != nil {
+		return nil
+	}
+
+	return payload
+}
+
+func (c *Controller) IsAuthorized(r *http.Request) bool {
+	token := strings.Split(r.Header.Get("Authorization"), "Bearer ")[1]
+
+	if token == "" {
+		return false
+	}
+
+	isAuthorized := auth.VerifyToken(token)
+
+	return isAuthorized
+}
+
+func generateErrorMessages(statusCode int) string {
+	switch statusCode {
+	case http.StatusMethodNotAllowed:
+		return "Method not allowed"
+	case http.StatusBadRequest:
+		return "Invalid data format"
+	case http.StatusUnauthorized:
+		return "Unauthorized"
+	case http.StatusNotFound:
+		return "Not found"
+	case http.StatusInternalServerError:
+		return "Internal server error"
+	default:
+		return "Unknown error"
+	}
+}
+
+
+func (c *Controller) Response(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if data == nil {
+		http.Error(w, generateErrorMessages(statusCode), statusCode)
+		return
+	}
+
+	switch v := data.(type) {
+	case string:
+		w.WriteHeader(statusCode)
+		w.Write([]byte(v))
+	case []byte:
+		w.WriteHeader(statusCode)
+		w.Write(v)
+	default:
+		http.Error(w, "Unsupported data type", http.StatusInternalServerError)
+	}
+}
+
+func (c *Controller) IsAllowedMethod(r *http.Request, methods []string) bool {
+	for _, method := range methods {
+		if r.Method == method {
+			return true
+		}
+	}
+
+	return false
 }
 
 func NewController(methods []ControllerMethod) *Controller {
