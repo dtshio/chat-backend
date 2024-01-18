d76b8fe2129a60461e6e514363ecc3969a45addd
datsfilipe <datsfilipe.foss@proton.me>
2024-01-18 02:05:07 -0300

    refactor: set routes properly

--- pkg/application/server/serveMux.go
+++ pkg/application/server/serveMux.go
@@ -9,16 +9,23 @@ import (
 func ServeMux(
 	userController *controllers.UserController,
 	messageController *controllers.MessageController,
-	channelController *controllers.ChannelController,
+	friendshipController *controllers.FriendshipController,
 ) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.Handle("/signup", CorsMiddleware(http.HandlerFunc(userController.HandleSignUp)))
 	mux.Handle("/signin", CorsMiddleware(http.HandlerFunc(userController.HandleSignIn)))
+
 	mux.Handle("/message", CorsMiddleware(http.HandlerFunc(messageController.HandleNewMessage)))
 	mux.Handle("/message/history", CorsMiddleware(http.HandlerFunc(messageController.HandleGetMessages)))
-	mux.Handle("/channel", CorsMiddleware(http.HandlerFunc(channelController.HandleNewChannel)))
-	mux.Handle("/channel/list", CorsMiddleware(http.HandlerFunc(channelController.HandleGetChannels)))
+
+	mux.Handle("/friendship/list", CorsMiddleware(http.HandlerFunc(friendshipController.HandleGetFriendships)))
+	mux.Handle("/friendship/delete", CorsMiddleware(http.HandlerFunc(friendshipController.HandleDeleteFriendship)))
+
+	mux.Handle("/friendship-requests", CorsMiddleware(http.HandlerFunc(friendshipController.HandleNewFriendshipRequest)))
+	mux.Handle("/friendship-requests/accept", CorsMiddleware(http.HandlerFunc(friendshipController.HandleNewFriendship)))
+	mux.Handle("/friendship-requests/list", CorsMiddleware(http.HandlerFunc(friendshipController.HandleGetFriendshipRequests)))
+	mux.Handle("/friendship-requests/delete", CorsMiddleware(http.HandlerFunc(friendshipController.HandleDeleteFriendshipRequest)))
 
 	return mux
 }
