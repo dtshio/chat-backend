a24a929402b6fab17c6c8a2ceaead98e6ffa020d
datsfilipe <datsfilipe.foss@proton.me>
2024-01-18 15:27:12 -0300

    feat: add route helper function

--- pkg/application/server/serveMux.go
+++ pkg/application/server/serveMux.go
@@ -6,6 +6,21 @@ import (
 	"github.com/datsfilipe/pkg/controllers"
 )
 
+func Route(
+	mux *http.ServeMux,
+	path string,
+	handler http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler,
+) {
+	var handlerWithMiddlewares http.Handler = handler
+
+	for _, middleware := range middlewares {
+		handlerWithMiddlewares = middleware(handlerWithMiddlewares)
+	}
+
+	mux.Handle(path, handlerWithMiddlewares)
+}
+
 func ServeMux(
 	userController *controllers.UserController,
 	messageController *controllers.MessageController,
@@ -13,19 +28,19 @@ func ServeMux(
 ) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.Handle("/signup", CorsMiddleware(http.HandlerFunc(userController.HandleSignUp)))
-	mux.Handle("/signin", CorsMiddleware(http.HandlerFunc(userController.HandleSignIn)))
+	Route(mux, "/signup", userController.HandleSignUp, CorsMiddleware)
+	Route(mux, "/signin", userController.HandleSignIn, CorsMiddleware)
 
-	mux.Handle("/message", CorsMiddleware(http.HandlerFunc(messageController.HandleNewMessage)))
-	mux.Handle("/message/history", CorsMiddleware(http.HandlerFunc(messageController.HandleGetMessages)))
+	Route(mux, "/message", messageController.HandleNewMessage, CorsMiddleware, AuthMiddleware)
+	Route(mux, "/message/history", messageController.HandleGetMessages, CorsMiddleware, AuthMiddleware)
 
-	mux.Handle("/friendship/list", CorsMiddleware(http.HandlerFunc(friendshipController.HandleGetFriendships)))
-	mux.Handle("/friendship/delete", CorsMiddleware(http.HandlerFunc(friendshipController.HandleDeleteFriendship)))
+	Route(mux, "/friendship/list", friendshipController.HandleGetFriendships, CorsMiddleware, AuthMiddleware)
+	Route(mux, "/friendship/delete", friendshipController.HandleDeleteFriendship, CorsMiddleware, AuthMiddleware)
 
-	mux.Handle("/friendship-requests", CorsMiddleware(http.HandlerFunc(friendshipController.HandleNewFriendshipRequest)))
-	mux.Handle("/friendship-requests/accept", CorsMiddleware(http.HandlerFunc(friendshipController.HandleNewFriendship)))
-	mux.Handle("/friendship-requests/list", CorsMiddleware(http.HandlerFunc(friendshipController.HandleGetFriendshipRequests)))
-	mux.Handle("/friendship-requests/delete", CorsMiddleware(http.HandlerFunc(friendshipController.HandleDeleteFriendshipRequest)))
+	Route(mux, "/friendship-requests", friendshipController.HandleNewFriendshipRequest, CorsMiddleware, AuthMiddleware)
+	Route(mux, "/friendship-requests/accept", friendshipController.HandleNewFriendship, CorsMiddleware, AuthMiddleware)
+	Route(mux, "/friendship-requests/list", friendshipController.HandleGetFriendshipRequests, CorsMiddleware, AuthMiddleware)
+	Route(mux, "/friendship-requests/delete", friendshipController.HandleDeleteFriendshipRequest, CorsMiddleware, AuthMiddleware)
 
 	return mux
 }
