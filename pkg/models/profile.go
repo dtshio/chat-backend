0009919de242b10ae08b9a521941d71d1a0fe01d
datsfilipe <datsfilipe.foss@proton.me>
2024-01-26 11:14:14 -0300

    refactor(profile): generate id in case it's not present
    - update before create record func

--- pkg/models/profile.go
+++ pkg/models/profile.go
@@ -1,13 +1,24 @@
 package models
 
+import "github.com/datsfilipe/pkg/core"
+
 type Profile struct {
   ID BigInt `json:"id" gorm:"primaryKey"`
   UserID BigInt `json:"user_id" gorm:"not null REFERENCES users(id)"`
   Username string `json:"username" gorm:"not null"`
 }
 
+func createID(id BigInt) BigInt {
+	if id != 0 {
+		return id
+	}
+
+	return BigInt(core.GenerateID())
+}
 
 func (p *Profile) BeforeCreateRecord(id BigInt) error {
+	p.ID = createID(id)
+
 	if p.ID < 0 {
 		p.ID = -p.ID
 	}
